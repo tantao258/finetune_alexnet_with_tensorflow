13810c0f751e3e8aac141e442537d8353c2af083
tantao258 <823232162@qq.com>
2018-12-08 15:07:30 +0800

    Update alexnet.py

--- alexnet.py
+++ alexnet.py
@@ -14,8 +14,7 @@ Link:
 - https://github.com/fastai/courses/blob/master/deeplearning1/nbs/vgg16.py
 
 
-The pretrained weights can be downloaded here and should be placed in the same
-folder as this file:
+The pretrained weights can be downloaded here and should be placed in the same folder as this file:
 - http://www.cs.toronto.edu/~guerzhoy/tf_alexnet/
 
 @author: Frederik Kratzert (contact: f.kratzert(at)gmail.com)
@@ -27,8 +26,7 @@ import numpy as np
 
 class AlexNet(object):
     """Implementation of the AlexNet."""
-
-    def __init__(self, x, keep_prob, num_classes, skip_layer, weights_path='DEFAULT'):
+    def __init__(self, keep_prob, num_classes, train_layers, learning_rate, weights_path='DEFAULT'):
         """Create the graph of the AlexNet model.
 
         Args:
@@ -41,31 +39,28 @@ class AlexNet(object):
                 isn't in the same folder as this code
         """
         # Parse input arguments into class variables
-        self.X = x
-        self.NUM_CLASSES = num_classes
-        self.KEEP_PROB = keep_prob
-        self.SKIP_LAYER = skip_layer
-
         if weights_path == 'DEFAULT':
             self.WEIGHTS_PATH = 'bvlc_alexnet.npy'
         else:
             self.WEIGHTS_PATH = weights_path
+        self.train_layers = train_layers
 
         # Call the create function to build the computational graph of AlexNet
-        self.create()
+        with tf.variable_scope("input"):
+            self.x_input = tf.placeholder(tf.float32, [None, 227, 227, 3], name="x_input")
+            self.y_input = tf.placeholder(tf.float32, [None, num_classes], name="y_input")
+            self.keep_prob = tf.placeholder(tf.float32, name="keep_prob")
 
-    def create(self):
-        """Create the network graph."""
         # 1st Layer: Conv (w ReLu) -> Lrn -> Pool
-        conv1 = conv(self.X, 11, 11, 96, 4, 4, padding='VALID', name='conv1')
+        conv1 = conv(self.x_input, 11, 11, 96, 4, 4, padding='VALID', name='conv1')
         norm1 = lrn(conv1, 2, 2e-05, 0.75, name='norm1')
         pool1 = max_pool(norm1, 3, 3, 2, 2, padding='VALID', name='pool1')
-        
+
         # 2nd Layer: Conv (w ReLu)  -> Lrn -> Pool with 2 groups
         conv2 = conv(pool1, 5, 5, 256, 1, 1, groups=2, name='conv2')
         norm2 = lrn(conv2, 2, 2e-05, 0.75, name='norm2')
         pool2 = max_pool(norm2, 3, 3, 2, 2, padding='VALID', name='pool2')
-        
+
         # 3rd Layer: Conv (w ReLu)
         conv3 = conv(pool2, 3, 3, 384, 1, 1, name='conv3')
 
@@ -77,20 +72,37 @@ class AlexNet(object):
         pool5 = max_pool(conv5, 3, 3, 2, 2, padding='VALID', name='pool5')
 
         # 6th Layer: Flatten -> FC (w ReLu) -> Dropout
-        flattened = tf.reshape(pool5, [-1, 6*6*256])
-        fc6 = fc(flattened, 6*6*256, 4096, name='fc6')
-        dropout6 = dropout(fc6, self.KEEP_PROB)
+        flattened = tf.reshape(pool5, [-1, 6 * 6 * 256])
+        fc6 = fc(flattened, 6 * 6 * 256, 4096, name='fc6')
+        dropout6 = dropout(fc6, keep_prob)
 
         # 7th Layer: FC (w ReLu) -> Dropout
         fc7 = fc(dropout6, 4096, 4096, name='fc7')
-        dropout7 = dropout(fc7, self.KEEP_PROB)
+        dropout7 = dropout(fc7, keep_prob)
 
         # 8th Layer: FC and return unscaled activations
-        self.fc8 = fc(dropout7, 4096, self.NUM_CLASSES, relu=False, name='fc8')
+        fc8 = fc(dropout7, 4096, num_classes, relu=False, name='fc8')
+
+        with tf.name_scope("loss"):
+            self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=fc8, labels=self.y_input))
+
+        with tf.name_scope("train"):
+            self.global_step = tf.Variable(0, name="global_step", trainable=False)
+            var_list = [v for v in tf.trainable_variables() if v.name.split('/')[0] in train_layers]
+            gradients = tf.gradients(self.loss, var_list)
+            self.grads_and_vars = list(zip(gradients, var_list))
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+            self.train_op = optimizer.apply_gradients(grads_and_vars=self.grads_and_vars, global_step=self.global_step)
+
+        with tf.name_scope("prediction"):
+            self.prediction = tf.argmax(fc8, 1, name="prediction")
+
+        with tf.name_scope("accuracy"):
+            correct_prediction = tf.equal(self.prediction, tf.argmax(self.y_input, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"), name="accuracy")
 
     def load_initial_weights(self, session):
         """Load weights from file into network.
-
         As the weights from http://www.cs.toronto.edu/~guerzhoy/tf_alexnet/
         come as a dict of lists (e.g. weights['conv1'] is a list) and not as
         dict of dicts (e.g. weights['conv1'] is a dict with keys 'weights' &
@@ -103,7 +115,7 @@ class AlexNet(object):
         for op_name in weights_dict:
 
             # Check if layer should be trained from scratch
-            if op_name not in self.SKIP_LAYER:
+            if op_name not in self.train_layers:
 
                 with tf.variable_scope(op_name, reuse=True):
 
@@ -123,17 +135,13 @@ class AlexNet(object):
 
 def conv(x, filter_height, filter_width, num_filters, stride_y, stride_x, name, padding='SAME', groups=1):
     """Create a convolution layer.
-
     Adapted from: https://github.com/ethereon/caffe-tensorflow
     """
     # Get number of input channels
     input_channels = int(x.get_shape()[-1])
 
     # Create lambda function for the convolution
-    convolve = lambda i, k: tf.nn.conv2d(input=i,
-                                         filter=k,
-                                         strides=[1, stride_y, stride_x, 1],
-                                         padding=padding)
+    convolve = lambda i, k: tf.nn.conv2d(input=i, filter=k, strides=[1, stride_y, stride_x, 1], padding=padding)
 
     with tf.variable_scope(name) as scope:
         # Create tf variables for the weights and biases of the conv layer
@@ -149,8 +157,7 @@ def conv(x, filter_height, filter_width, num_filters, stride_y, stride_x, name, 
     else:
         # Split input and weights and convolve them separately
         input_groups = tf.split(axis=3, num_or_size_splits=groups, value=x)
-        weight_groups = tf.split(axis=3, num_or_size_splits=groups,
-                                 value=weights)
+        weight_groups = tf.split(axis=3, num_or_size_splits=groups, value=weights)
         output_groups = [convolve(i, k) for i, k in zip(input_groups, weight_groups)]
 
         # Concat the convolved output together again
@@ -193,7 +200,8 @@ def max_pool(x, filter_height, filter_width, stride_y, stride_x, name, padding='
     return tf.nn.max_pool(value=x,
                           ksize=[1, filter_height, filter_width, 1],
                           strides=[1, stride_y, stride_x, 1],
-                          padding=padding, name=name)
+                          padding=padding,
+                          name=name)
 
 
 def lrn(x, radius, alpha, beta, name, bias=1.0):
